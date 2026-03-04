8cc31d64512cbe968ffa125f5aed4fa9cb93adda
LTWe-sports <learntowinesports@gmail.com>
2026-03-05 01:00:07 +0900

    Create Home component for main page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      background:"#0f0f0f",
+      color:"white",
+      minHeight:"100vh",
+      padding:"40px",
+      fontFamily:"Arial"
+    }}>
+      <h1 style={{fontSize:"40px"}}>TEAM PR RANKING</h1>
+      <p>サイト構築成功🔥</p>
+    </div>
+  );
+}
